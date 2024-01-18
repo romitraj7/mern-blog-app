3998a9a4ada3e64fc50e586ff2c0d0356b6f7fc3
Romit <romit2884@gmail.com>
2024-01-18 16:47:51 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log("database connected")})
+.catch((error)=>{console.log(error)})
 const app = express()
 const port = 3000;
 
