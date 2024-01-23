38c563edad776bfc3ee5df6eb9430c6b07728ea9
Romit <romit2884@gmail.com>
2024-01-23 18:13:46 +0530

    update user api roue

--- api/index.js
+++ api/index.js
@@ -1,24 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 // import cors from 'cors'
-
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{console.log("database connected")})
 .catch((error)=>{console.log(error)})
-const app = express()
-app.use(express.json())
+const app = express();
 const port = 3000;
-
+app.use(express.json());
 // app.use(cors())
-
+app.use(cookieParser());
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 // middleware for errors
+
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server error';
@@ -28,6 +27,7 @@ app.use((err,req,res,next)=>{
         message,
     })
 })
+
 app.listen(port,(req,res)=>{
     console.log(`Server is running on http://localhost:${port}`)
 });
