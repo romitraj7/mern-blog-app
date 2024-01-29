338383e4d1342d0dcb999aeb0f47417941e2f477
Romit <romit2884@gmail.com>
2024-01-30 01:04:23 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -23,11 +23,11 @@ app.use('/api/auth', authRouter);
 app.use('/api/post' ,postRouter);
 app.use('/api/comment',commentRouter);
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  });
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server error';
