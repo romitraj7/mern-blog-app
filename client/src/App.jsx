4081b98e818409275d539b852dc33a3e34bde2cb
Romit <romit2884@gmail.com>
2024-01-30 01:01:02 +0530

    updating app an header file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,43 @@
-import { BrowserRouter, Routes , Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import Projects from './pages/Projects'
-import Dashboard from './pages/Dashboard'
-import Header from './components/header'
-import FooterComponent from './components/FooterComponent'
-import PrivateRoute from './components/PrivateRoute'
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
-import CreatePost from './pages/CreatePost'
-import UpdatePost from './pages/UpdatePost'
-import PostPage from './pages/PostPage'
-import ScrollTop from './components/ScrollToTop'
-import Search from './pages/Search'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import FooterComponent from './components/FooterComponent';
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 export default function App() {
   return (
-   <BrowserRouter>
-    <ScrollTop />
-    <Header />
-    <Routes>
-      <Route path='/' element = {<Home></Home>} />
-      <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path = '/about' element={<About/>}/>
-      <Route path = '/sign-in' element={<SignIn/>}/>
-      <Route path = '/projects' element={<Projects/>}/>
-      <Route path = '/search' element={<Search/>}/>
-      <Route element = {<PrivateRoute/>}>
-      <Route path = '/dashboard' element={<Dashboard/>}/>
-      </Route>
-      <Route element = {<OnlyAdminPrivateRoute/>}>
-      <Route path = '/create-post' element={<CreatePost/>}/>
-      <Route path = '/update-post/:postId' element={<UpdatePost/>}/>
-      </Route>
-      <Route path='/post/:postSlug' element={<PostPage/>}/>
-    </Routes>
-    <FooterComponent/>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+        </Route>
+
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
+      </Routes>
+      <FooterComponent />
+    </BrowserRouter>
+  );
 }
