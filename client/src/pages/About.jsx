0e347ed53b5272592a38d89af223333443801e1d
romitraj7 <122794169+romitraj7@users.noreply.github.com>
2024-01-30 01:21:57 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,20 +4,20 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Romit' Blog
+            About Romit's Blog
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
               Welcome to Romit's Blog! This blog was created by Romit Raj Singh
               as a personal project to share his thoughts and ideas with the
-              world. Romit is a passionate developer who loves to write about
+              world.I am a passionate developer who loves to write about
               technology, coding, and everything in between.
             </p>
 
             <p>
               On this blog, you'll find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
+              languages.I am always learning and exploring new
               technologies, so be sure to check back often for new content!
             </p>
 
